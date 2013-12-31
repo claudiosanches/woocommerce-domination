20481baca1e0002535f4350b4927e3b691e2ae90
claudiosmweb <contato@claudiosmweb.com>
2013-12-31 00:25:20 -0200

    added initial methods in WooCommerce_Domination class

--- public/class-woocommerce-domination.php
+++ public/class-woocommerce-domination.php
@@ -16,11 +16,29 @@
 class WooCommerce_Domination {
 
 	/**
+	 * Plugin version, used for cache-busting of style and script file references.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var   string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Plugin slug.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var   string
+	 */
+	protected static $plugin_slug = 'woocommerce-domination';
+
+	/**
 	 * Instance of this class.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @var object
+	 * @var   object
 	 */
 	protected static $instance = null;
 
@@ -28,7 +46,11 @@ class WooCommerce_Domination {
 	 * Initialize the plugin.
 	 */
 	private function __construct() {
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
 
+		// Activate plugin when new blog is added
+		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
 	}
 
 	/**
@@ -45,5 +67,169 @@ class WooCommerce_Domination {
 		}
 
 		return self::$instance;
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return Plugin slug variable.
+	 */
+	public function get_plugin_slug() {
+		return self::$plugin_slug;
+	}
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param  boolean $network_wide True if WPMU superadmin uses
+	 *                               "Network Activate" action, false if
+	 *                               WPMU is disabled or plugin is
+	 *                               activated on an individual blog.
+	 *
+	 * @return void
+	 */
+	public static function activate( $network_wide ) {
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+			if ( $network_wide  ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+
+					switch_to_blog( $blog_id );
+					self::single_activate();
+				}
+
+				restore_current_blog();
+			} else {
+				self::single_activate();
+			}
+		} else {
+			self::single_activate();
+		}
+	}
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param  boolean $network_wide True if WPMU superadmin uses
+	 *                               "Network Deactivate" action, false if
+	 *                               WPMU is disabled or plugin is
+	 *                               deactivated on an individual blog.
+	 *
+	 * @return void
+	 */
+	public static function deactivate( $network_wide ) {
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+			if ( $network_wide ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+					switch_to_blog( $blog_id );
+					self::single_deactivate();
+				}
+
+				restore_current_blog();
+			} else {
+				self::single_deactivate();
+			}
+		} else {
+			self::single_deactivate();
+		}
+	}
+
+	/**
+	 * Fired when a new site is activated with a WPMU environment.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param  int $blog_id ID of the new blog.
+	 *
+	 * @return void
+	 */
+	public function activate_new_site( $blog_id ) {
+		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+			return;
+		}
+
+		switch_to_blog( $blog_id );
+		self::single_activate();
+		restore_current_blog();
+	}
+
+	/**
+	 * Get all blog ids of blogs in the current network that are:
+	 * - not archived
+	 * - not spam
+	 * - not deleted
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return array|false The blog ids, false if no matches.
+	 */
+	private static function get_blog_ids() {
+		global $wpdb;
+
+		// get an array of blog ids
+		$sql = "SELECT blog_id FROM $wpdb->blogs
+			WHERE archived = '0' AND spam = '0'
+			AND deleted = '0'";
+
+		return $wpdb->get_col( $sql );
+	}
+
+	/**
+	 * Fired for each blog when the plugin is activated.
+	 *
+	 * @since 1.0.0
+	 */
+	private static function single_activate() {
+
+	}
+
+	/**
+	 * Fired for each blog when the plugin is deactivated.
+	 *
+	 * @since 1.0.0
+	 */
+	private static function single_deactivate() {
+
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since  2.8.0
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		$domain = self::$plugin_slug;
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+	}
+
+	/**
+	 * Test with WooCommerce is activated.
+	 *
+	 * @return bool
+	 */
+	public static function is_woocommerce() {
+		if ( function_exists( 'WC' ) ) {
+			return true;
+		}
+
+		return false;
 	}
 }
--- woocommerce-domination.php
+++ woocommerce-domination.php
@@ -25,11 +25,21 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// Public.
+/**
+ * Public.
+ */
 require_once( plugin_dir_path( __FILE__ ) . 'public/class-woocommerce-domination.php' );
 add_action( 'plugins_loaded', array( 'WooCommerce_Domination', 'get_instance' ) );
 
-// Admin.
+/**
+ * PLugin activate and deactivate methods.
+ */
+register_activation_hook( __FILE__, array( 'WooCommerce_Domination', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'WooCommerce_Domination', 'deactivate' ) );
+
+/**
+ * Admin.
+ */
 if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-woocommerce-domination-admin.php' );
 	add_action( 'plugins_loaded', array( 'WooCommerce_Domination_Admin', 'get_instance' ) );
